59d8db214000ae10ba939ef253e96046b057d648
per-aspera-a-d-astra <bjlsnowy@163.com>
2025-05-01 12:24:31 -0500

    Update app_credit_card.py

--- app_credit_card.py
+++ app_credit_card.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 
 # Set page configuration
 st.set_page_config(page_title="Credit Risk Prediction Demo", page_icon="💰", layout="wide")
